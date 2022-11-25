7e1b639e1897f1f7ba2c4585fade0c8252638e93
pengyinghao <pengyinghao@pengyinghao>
2022-11-25 17:17:54 +0800

    添加可拖拽tabs及canvasUtils

--- mystatic/assets/case/squared/index.vue
+++ mystatic/assets/case/squared/index.vue
@@ -6,17 +6,17 @@
   </div>
   <div class="mt24">
     <span>对应九宫格：</span>
-    <img :src="squaredUrl">
+    <canvas ref="myCanvas"></canvas>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { nextTick, ref } from 'vue'
 import squared from './index';
 const squaredUrl = ref('')
-squared('/src/assets/img/squared.png',500, 100, 10, 10, 10, 10).then(res=>{
-  squaredUrl.value = res
-}).catch(err=>{
-  console.log(err);
+const myCanvas = ref(null)
+nextTick(()=>{
+  const s = new squared(myCanvas.value,500,100)
+  s.drawSquaredImg('/src/assets/img/squared.png',500, 100, 10, 10, 10, 10)
 })
 </script>
